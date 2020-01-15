4c0eaf3f9ee0c170497732ed9a2bea130a547acf
Jack T <jackmt9@gmail.com>
2020-01-15 03:35:15 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -18,6 +18,11 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  array.sort!
-  array[0]
+  max = 0
+  array.length.times do |index|
+    if max < array[index]
+      max = array[index]
+    end
+  end
+  return max
 end
